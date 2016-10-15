fb8d34945333c63371f017b4eaf7ed5b403a0ca4
Lewis Johnson <LewisJohnsonDev@gmail.com>
2016-10-15 20:03:38 +0100

    Update GetClientInfo.cs

--- ImgurLibrary/GetClientInfo.cs
+++ ImgurLibrary/GetClientInfo.cs
@@ -4,6 +4,6 @@
     {
         public static string ClientId => "6e1cd0a5bdb4786";
 
-        public static string Secret => "06fc87ec29d92a2b4e91025cf3c788c9f33cb11e";
+        public static string Secret => ;
     };
 }
